7f5baecfa67a7aacb3947db7278b662f5b314962
Michał Ganiebny <michalganiebny@rocketmail.com>
2022-10-13 12:57:11 +0200

    Initial Commit

---
+++ module4lesson15/module4lesson15App.swift
@@ -0,0 +1,17 @@
+//
+//  module4lesson15App.swift
+//  module4lesson15
+//
+//  Created by Michał Ganiebny on 13/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct module4lesson15App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
